e1a783fb0c641aa1ed2fb8a2284b67509d59caa8
Twins Admin <webmaster@edinburghtwins.co.uk>
2010-05-26 22:52:14 +0100

    Wrapped paragraphs of committee page, prior to content editing.

--- aboutus/committee/index.php
+++ aboutus/committee/index.php
@@ -8,38 +8,91 @@
     </div>
     <div id="content">
     <h1 class="header">The Committee</h1>
-        <p>
-        <strong>
-     The club is run by a Committee of volunteers.  The duties of each committee post are detailed below.  We are always on the look out for new committee members, so please contact us if you would like to find out more. 
-        </strong>
-        </p>
-<p><strong>Chairperson </strong>
-Responsible for making sure that the club is working towards its aims of supporting and celebrating multiple birth families in Edinburgh and the Lothians.  
-Maintaining/increasing our contact with the local hospitals, and health centres.  
-Committee meetings are held every six weeks and preparing agendas and bringing along copies of minutes usually falls to the Chairperson. 
-Roping members both on and off the committee into helping out.
-Instrumental in the orgainsation of the annual Summer Fair and Christmas party.
-</p>  
-<p><strong>Secretary</strong> Take minutes at committee meetings and generally support the committee and the club throughout the year.
-This would be an ideal role for anyone wanting either to join or stay on the committee and undertake a role that is enjoyable but not too onerous!</p>
-<p><strong>Treasurer </strong>The main duties involve banking cheques, writing cheques and updating the records to produce an income and expenditure account for the year.</p>
-<p><strong>Fundraiser </strong>Responsible for raising funds through a variety of means.  This includes running the nearly new sales and approaching businesses for raffle prices or donations for specific equipment.</p>
-<p><strong>Membership Secretary </strong>This role is very flexible and can be shared by two people.
-The main tasks are administration of the Excel membership database, produce quarterly membership reports for the newsletter, produce short updeates for each committee meeting, manage the annual renewal of each family's membership, send out welcome packs to our new members and put them in contact with other members who live nearby or have twins at the same stage.
-Also responsible for collecting all the information for the monthly email and sending it out to members.</p>
-<p><strong>Social Convenor </strong>Although usually filled by two committee members, only one is in post at present (any volunteers?).
-The main responsibility is to organise a range of events throughout the year for multiples, their siblings and parents.
-As well as getting to plan the social calendar for our members, making and confirming the booking with venues, you are also responsible for taking payment for event bookings from members and forwarding the monies on to our Treasurer, as well as paying deposits and balances to venues (always promptly reimbursed by our Treasurer!). 
-It is a great position for keeping your organisational skills up to scratch! A full list of contact details for previously booked events will be provided.
+
+<p><strong> The club is run by a Committee of volunteers.  The duties
+of each committee post are detailed below.  We are always on the look
+out for new committee members, so please contact us if you would like
+to find out more.</strong> </p>
+
+<p><strong>Chairperson </strong> Responsible for making sure that the
+club is working towards its aims of supporting and celebrating
+multiple birth families in Edinburgh and the Lothians.
+Maintaining/increasing our contact with the local hospitals, and
+health centres.  Committee meetings are held every six weeks and
+preparing agendas and bringing along copies of minutes usually falls
+to the Chairperson.  Roping members both on and off the committee into
+helping out.  Instrumental in the orgainsation of the annual Summer
+Fair and Christmas party.  </p>
+
+<p><strong>Secretary</strong> Take minutes at committee meetings and
+generally support the committee and the club throughout the year.
+This would be an ideal role for anyone wanting either to join or stay
+on the committee and undertake a role that is enjoyable but not too
+onerous!</p>
+
+<p><strong>Treasurer </strong>The main duties involve banking cheques,
+writing cheques and updating the records to produce an income and
+expenditure account for the year.</p>
+
+<p><strong>Fundraiser </strong>Responsible for raising funds through a
+variety of means.  This includes running the nearly new sales and
+approaching businesses for raffle prices or donations for specific
+equipment.</p>
+
+<p><strong>Membership Secretary </strong>This role is very flexible
+and can be shared by two people.  The main tasks are administration of
+the Excel membership database, produce quarterly membership reports
+for the newsletter, produce short updeates for each committee meeting,
+manage the annual renewal of each family's membership, send out
+welcome packs to our new members and put them in contact with other
+members who live nearby or have twins at the same stage.  Also
+responsible for collecting all the information for the monthly email
+and sending it out to members.</p>
+
+<p><strong>Social Convenor </strong>Although usually filled by two
+committee members, only one is in post at present (any volunteers?).
+The main responsibility is to organise a range of events throughout
+the year for multiples, their siblings and parents.  As well as
+getting to plan the social calendar for our members, making and
+confirming the booking with venues, you are also responsible for
+taking payment for event bookings from members and forwarding the
+monies on to our Treasurer, as well as paying deposits and balances to
+venues (always promptly reimbursed by our Treasurer!).  It is a great
+position for keeping your organisational skills up to scratch! A full
+list of contact details for previously booked events will be provided.
 </p>
-<p><strong>Daytime Coordinator, Oxgangs </strong>Duties entail welcoming newcomers, collecting the money, paying for the hall and making sure there are enough biscuits. There are always other people happy to help out if I can't make it, and people to scoop up and cuddle my babies if I'm busy with the books. </p>
-<p><strong>Daytime Coordinator, South Queensferry </strong>The role involves opening and locking the hall, putting out the toys, making sure we have enough teabags, milk and biscuits, taking money and giving the Treasurer an account of ins and outs.  There are also posters to make sometimes and forms to print.</p>
-<p><strong>Daytime Coordinator, Longniddry </strong>A few days before the group meets, email the members from East Lothian just to remind them it is on, and any other new things that might be happening, if we have a speaker or someone coming along.  Get the toys out from the toy cupboard and wait for everyone to arrive, introducing yourself and other families to new members.
-Liasing with anyone who has something they wish to sell, or if anyone has any questions - directing these to the mum most likely to know the answer.  Tidy the toys at the end of the session and remind everyone when we the next group will be held.</p>
-<p><strong>Newsletter Editor </strong>This role involves cajoling the committee and others into writing articles, word processing the newsletter in a standard format and submitting it on to a printer.</p>
+
+<p><strong>Daytime Coordinator, Oxgangs </strong>Duties entail
+welcoming newcomers, collecting the money, paying for the hall and
+making sure there are enough biscuits. There are always other people
+happy to help out if I can't make it, and people to scoop up and
+cuddle my babies if I'm busy with the books. </p>
+
+<p><strong>Daytime Coordinator, South Queensferry </strong>The role
+involves opening and locking the hall, putting out the toys, making
+sure we have enough teabags, milk and biscuits, taking money and
+giving the Treasurer an account of ins and outs.  There are also
+posters to make sometimes and forms to print.</p>
+
+<p><strong>Daytime Coordinator, Longniddry </strong>A few days before
+the group meets, email the members from East Lothian just to remind
+them it is on, and any other new things that might be happening, if we
+have a speaker or someone coming along.  Get the toys out from the toy
+cupboard and wait for everyone to arrive, introducing yourself and
+other families to new members.  Liasing with anyone who has something
+they wish to sell, or if anyone has any questions - directing these to
+the mum most likely to know the answer.  Tidy the toys at the end of
+the session and remind everyone when we the next group will be
+held.</p>
+
+<p><strong>Newsletter Editor </strong>This role involves cajoling the
+committee and others into writing articles, word processing the
+newsletter in a standard format and submitting it on to a printer.</p>
+
 <p><strong>Webmaster </strong>Keep the website online and up to date.
-Moderate the messageboard, assisting users and setting up new accounts.  Setup and maintain the club email accounts.</p>
-<h2></h2>
+Moderate the messageboard, assisting users and setting up new
+accounts.  Setup and maintain the club email accounts.</p>
+
     </div>
     <div class="clear">
       <br />
