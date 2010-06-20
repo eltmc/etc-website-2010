758e83c362a5b8e4d9bd763f192d80bbeb25e1e8
Nick Stokoe <etc-www-git.5.wu-lee@spamgourmet.org>
2010-06-20 11:50:56 +0100

    Replaced application-form.doc with a new PDF version
    This one has the correct address for the new Membership coordinator.

--- join/index.php
+++ join/index.php
@@ -20,7 +20,8 @@ If you join the Edinburgh Twins Club (for the small cost of £10 pa), you will g
   <li>Library of books and leaflets</li>
 </ul>        </p>
 <p>
-Download <a href="http://www.edinburghtwins.co.uk/documents/application-form.doc">application form</a>
+Download <a href="http://www.edinburghtwins.co.uk/documents/application-form.pdf">application form</a>
+
 </p>
 <hr width=100% noshade size=1>
 <center>
