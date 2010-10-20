d386d8072af8703b317f26992cc68d4def165e84
Nick Stokoe <etc-www-git.5.wu-lee@spamgourmet.org>
2010-10-20 22:28:01 +0100

    Removed cancelled Doodles event

--- events/index.php
+++ events/index.php
@@ -18,25 +18,6 @@ event locations.</p>
 href="mailto:social@edinburghtwins.co.uk">social@edinburghtwins.co.uk</a></p>
 
 
-<h2>Doodles Ceramic Experience: September 16th, 8pm - <b>CANCELLED</b></h2>
-
-<p><i>Unfortunately this has been called off due to insufficient interest.</i></p>
-
-<div style="text-decoration: line-through;">
-<p><i>Donna says:</i> Join us for a night of creativity... Or, if you are as artistic as me, maybe just an evening of fun with the chance to relax.</p>
-
-<p>Please let me know if you are able to make it ASAP so I can make sure that we have space booked!</p>
- 
-<p style="margin-left: 2em">
-
-  <b>Where:</b>
-  <span><a href="http://www.doodlesscotland.co.uk/location/">Doodles Ceramic Experience</a> - Marchmount (<a href="http://maps.google.com/maps/place?q=doodles+ceramic+workshop+edinburgh&cid=15743022771082384977">map</a>)</span><br>
-  <b>When:</b>
-  <span>September 16th, 20:00h</span><br>
-  <b>Entry:</b>
-  <span>Not for kids, just adults!</span><br>
-
-</p> 
 </div>
     </div>
     <div class="clear">
