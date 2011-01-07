28a86c6a037b58582e2bd5b289c532e964148bed
Nick Stokoe <etc-www-git.5.wu-lee@spamgourmet.org>
2011-01-07 14:38:38 +0000

    Removed closure announcement of SQF BBT

--- events/index.php
+++ events/index.php
@@ -23,6 +23,29 @@ event locations.</p>
 - email: <a
 href="mailto:social@edinburghtwins.co.uk">social@edinburghtwins.co.uk</a></p>
 
+<h2>Kids Ceilidh: Saturday 26th February, 10am - 12am</h2>
+
+<p>Come and join us for a fun winter warmer: the very first ETC kids' ceilidh!</p>
+
+<p>For further information, and to sign up,
+please contact Donna and Vikki at: <a
+href="mailto:social@edinburghtwins.co.uk">social@edinburghtwins.co.uk</a>
+(please let us know before 19th Feb if you wish to attend).</p>
+
+<p>Information on CeilidhKids can be found at: <a
+href="http://www.ceilidhkids.com">www.ceilidhkids.com</a></p>
+
+
+<p style="margin-left: 2em">
+
+  <b>Where:</b>
+  <span>St Thomas' Church, 79 Glasgow Road, Corstorphine, Edinburgh, EH12 8LJ (<a href="http://maps.google.co.uk/maps/place?cid=7326416247164003070">map</a>)</span><br>
+  <b>When:</b>
+  <span>Saturday February 26th, 10:00am - 12:00pm; A snack lunch will be provided.</span><br>
+  <b>Entry:</b>
+  <span>&pound;4 per participating child (non-dancers free)</span><br>
+
+</p> 
 
     <div class="clear">
       <br />
