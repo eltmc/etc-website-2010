8263e8ada573cd39a08be06ca7eef4ef83e986a3
Nick Stokoe <etc-www-git.5.wu-lee@spamgourmet.org>
2011-03-04 23:31:08 +0000

    Removed announcements about ceilidh and late winter newsletter

--- index.php
+++ index.php
@@ -38,15 +38,6 @@
               Group will be relocating</b> - see the <a href="groups">Groups
               page</a></li><br>
 
-              <li>Join us for the very first <b>Kids Ceilidh: Saturday
-              26th February, 10am - 12am</b> see the <a
-              href="events">Events page</a> for details.</li><br>
-            
-              <li>Apologies! <b>The Winter Newsletter will be
-              delayed.</b> Please see the announcement on the
-              messageboard <a
-              href="/messageboard/viewtopic.php?f=3&t=382">here</a>.</li><br>
-
               <li><b>Got the last newsletter? No? Check your
               membership.</b> See <a
               href="http://www.edinburghtwins.co.uk/messageboard/viewtopic.php?f=3&t=327">this
