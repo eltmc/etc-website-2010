467ea10b046e326b3e2a00475a790cb977cff29a
Nick Stokoe <nospamnick@noodlefactory.co.uk>
2011-03-11 23:33:08 +0000

    no-changes.t - check we've not changed anything significant

---
+++ t/data/no-changes/html/events/map/eventdata.js
@@ -0,0 +1,10 @@
+// Further info for each event 
+
+dataObj['NN']='Nearly New Sale: St. Stephens Church 12th September,<br>10am start for members, 10:30 - Public Entry';
+dataColorObj['NN']='green';
+dataObj['AD']='2009 Edinburgh Twins Annual Dinner 26th September,<br>The Merlin, Morningside, Edinburgh - 19:30';
+dataColorObj['AD']='green';
+dataObj['MD']='Mums or Dads night out, 8pm on Friday 19th March,<br> Caley Sample Rooms, Angle Terrace, Edinburgh. ';
+dataColorObj['MD']='green';
+dataObj['EG']='Twins Club Easter Egg Hunt, 10:45am on Saturday 10th April,<br> Vogrie Country Park, Midlothian';
+dataColorObj['EG']='green';
