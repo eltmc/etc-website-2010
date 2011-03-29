8e1ed28f6749cf4f8a5bf6d4a20bcf1481b0b8cd
Nick Stokoe <nospamnick@noodlefactory.co.uk>
2011-03-30 00:38:36 +0100

    ./index.php - remove an old announcement of 1st years which is wrong
    And lightly edit the other announcements to fit.

--- t/data/no-changes/html/index.php
+++ t/data/no-changes/html/index.php
@@ -130,21 +130,18 @@
         <div class="announcements">
           <h2>Announcements</h2>
           <ul>
-
-              <li><b>The Joppa BBT group will not be meeting on Monday
-              14th 2011,</b> but there is an alternative plan to meet at
-              the park, weather permitting - see <a
-              href="messageboard/viewtopic.php?f=5&amp;t=442">this
-              messageboard post</a> and <a
-              href="messageboard/viewtopic.php?f=5&amp;t=440">this one</a>
-              for more details.</li>
+              <li>The <b>next First Years group is on 6th April at St
+              John's Hospital</b> and will cover "Weaning and
+              Feeding". See <a href="messageboard/viewtopic.php?f=7&amp;t=443">this
+              message board post</a>.</li>
 
               <li>Following the recent survey, since 2nd March 2011,
-              <b>First Years now runs on the first Wednesday of
-              each month, alternating between Oxgangs Neighbourhood
-              Centre in Edinburgh and St John's Hospital.</b> See the
-              <a href="groups/#FirstYears">groups page</a>
-              for more details and directions.</li>
+              <b><a href="groups/#FirstYears">First Year's</a> group
+              now runs on the first Wednesday of each month</b>,
+              alternating between Oxgangs Neighbourhood Centre in
+              Edinburgh and St John's Hospital. See the <a
+              href="groups/#FirstYears">groups page</a> more details
+              and directions.</li>
 
 
               <li>From Friday 18th February the <b>South Queensferry
@@ -156,22 +153,9 @@
               href="messageboard/viewtopic.php?f=3&amp;t=327">this
               notice</a> </li>
 
-              <li>Expecting twins? Maybe your babies have arrived?
-              Come along to <b><a
-              href="groups/#FirstYears">First
-              Year's</a> group</b>, at 8pm on the first Wednesday of
-              the month in St John's Hospital, Livingston, or 8pm on
-              the second Tuesday at Oxgangs Neighbourhood Centre.
-              Watch for announcements of upcoming meetings on our <a
-              href="messageboard/viewforum.php?f=7">First Years</a>
-              messageboard.</li>
-
-
-              <li>And don't forget the regular <b>Babies, Bumps and
-              Toddler</b> groups! See the <b><a
-              href="groups">Groups</a></b>
-              page for details, including information about
-              non-Twins-Club babies, bumps and toddler groups</li>
+              <li>See the <b><a href="groups">Groups</a></b> page for
+              information about Babies Bumps and Toddler groups,
+              including non-Edinburgh-Twins-Club twin groups.</li>
 
           </ul>
         </div>
