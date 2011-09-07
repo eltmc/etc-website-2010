84591b82b6757c15e1bbb48895547a1a1ab5c4ea
Twins Admin <webmaster@edinburghtwins.co.uk>
2011-09-07 23:47:07 +0100

    Update the notice on the messageboard page.

--- src/messageboard.php
+++ src/messageboard.php
@@ -9,13 +9,9 @@
 
 <h2>Important Notice For New Users</h2>
 <p>
-If you are looking to register for the message boards, please complete the registration form on the message board and then email your username to <a href="mailto:webmaster@edinburghtwins.co.uk">webmaster@edinburghtwins.co.uk</a> to confirm registration. Thanks for your cooperation.
+If you are looking to register for the message boards, please complete the registration form on the message board and then email your username to <a href="mailto:webmaster@edinburghtwins.co.uk">webmaster@edinburghtwins.co.uk</a> to confirm registration.  This is because we receive many bogus automated registrations every day, and the notifications are being junked until we can find an effective way of stopping them.  Apologies for the inconvenience.
 </p>
 
-<h2>Important Notice Regarding the Message Board</h2>
-<p>
-We recently had an issue with our message board which meant all previous posts were lost. If you had an account setup previously, you will need to re-register. If you follow the above note your registration will be processed quicker. We apologise for the inconvenience this causes you.
-</p>
 <p>
 <a href="messageboard/">Continue to the Message Board</a>
 </p>
