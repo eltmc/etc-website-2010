cc24b94a249cb488210a1a9d7d863bd729f2dfa0
Twins Admin <webmaster@edinburghtwins.co.uk>
2012-06-15 11:11:47 +0100

    local_config.php - a partial phpBB config file, minus the password
    The original config.php has been removed from this repository, since
    the repository may be stored in a public place (github).
    
    To restore it, copy local_config.php as config.php and insert the
    correct password.

---
+++ messageboard/local_config.php
@@ -0,0 +1,20 @@
+<?php
+// To restore config.php, copy this file as config.php 
+// and insert the correct value for $dbpasswd
+
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysql';
+$dbhost = '';
+$dbport = '';
+$dbname = 'test';
+$dbuser = 'twinsadmin';
+$dbpasswd = 'XXXXXXXXX';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
